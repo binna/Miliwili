15ac3b0541228b84f5db4e9e643d6aae49550316
binna <every5116@naver.com>
2021-03-07 14:30:42 +0900

    Template 수정

--- src/main/java/com/app/miliwili/utils/ValidationRegex.java
+++ src/main/java/com/app/miliwili/utils/ValidationRegex.java
@@ -1,11 +1,28 @@
 package com.app.miliwili.utils;
 
+import org.springframework.stereotype.Service;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@Service
 public class ValidationRegex {
     public static boolean isRegexEmail(String target) {
         String regex = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(target);
+        return matcher.find();
+    }
+
+    public static boolean isRegexPhoneNumber(String target) {
+        String regex = "^\\d{3}-\\d{3,4}-\\d{4}$";
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(target);
+        return matcher.find();
+    }
+
+    public static boolean isRegexDate(String target) {
+        String regex = "^(19|20)\\d{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[0-1])$";
         Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(target);
         return matcher.find();
