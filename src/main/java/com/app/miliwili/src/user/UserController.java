9c70bcef2fc9ab14f6f5dfa52058a6e88acfca46
binna <every5116@naver.com>
2021-03-09 12:15:08 +0900

    jwt 수정, User 템플릿 추가

---
+++ src/main/java/com/app/miliwili/src/user/UserController.java
@@ -0,0 +1,38 @@
+package com.app.miliwili.src.user;
+
+import com.app.miliwili.config.BaseException;
+import com.app.miliwili.config.BaseResponse;
+import com.app.miliwili.utils.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.app.miliwili.config.BaseResponseStatus.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("")
+public class UserController {
+    private final JwtService jwtService;
+
+    /**
+     * JWT 검증 API
+     * [GET]
+     * @RequestHeader jwt
+     * @return BaseResponse<Void>
+     * @Auther shine
+     */
+    @GetMapping("/users/jwt")
+    public BaseResponse<Void> jwt(@RequestHeader(value = "jwt") String jwt) {
+        try {
+            Long userId = jwtService.getUserId();
+            // TODO 중복된 회원조회 필요 -> 회원가입 API 만든 후 만들 예정
+            return new BaseResponse<>(SUCCESS);
+        } catch (BaseException exception) {
+            return new BaseResponse<>(exception.getStatus());
+        }
+    }
+
+}
