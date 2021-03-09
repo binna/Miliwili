410d2979d7bdd18e2c49a379e133d5ba827feed7
binna <every5116@naver.com>
2021-03-09 15:18:08 +0900

    user 생일, 목표날짜 TIMESTAMP -> DATE 변경, promotionStatus 테이블 추가

---
+++ src/main/java/com/app/miliwili/src/user/models/PromotionState.java
@@ -0,0 +1,42 @@
+package com.app.miliwili.src.user.models;
+
+import com.app.miliwili.config.BaseEntity;
+import lombok.*;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Accessors(chain = true)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@EqualsAndHashCode(callSuper = false)
+@Data
+@Entity
+@Table(name = "promotionState")
+public class PromotionState extends BaseEntity {
+    @Id
+    @Column(name = "id", nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "firstDate", nullable = false)
+    private LocalDate firstDate;
+
+    @Column(name = "secondDate")
+    private LocalDate secondDate;
+
+    @Column(name = "thirdDate")
+    private LocalDate thirdDate;
+
+    @Column(name = "startDate", nullable = false)
+    private LocalDate startDate;
+
+    @Column(name = "endDate", nullable = false)
+    private LocalDate endDate;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
